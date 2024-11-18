7296655c4c9857a8142eb6da9d16b63ffd4ad208
Sergiobau15 <159957565+Sergiobau15@users.noreply.github.com>
2024-11-18 17:33:16 -0500

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"proyecto"
+    host:process.env.HOSTDB || "localhost",
+    user: process.env.USERDB || "root",
+    password: process.env.PASSWORDDB || "",
+    database: process.env.DB || "Proyectoo",
+    port: process.env.PORTDB || 3306
 });
 
 db.connect(err => {
